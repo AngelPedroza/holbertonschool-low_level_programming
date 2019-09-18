a29554d0c879b2f915ebb23b12281f8ca95d2291
AngelPedroza <angelomarpedrozacardenas@gmail.com>
2019-09-18 22:49:46 +0000

    [Fix]Return 1

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,10 +2,10 @@
 /**
 * main - Body of the program
 *
-* Return: Always is 0
+* Return: Return 1 for this example
 */
 int main(void)
 {
 	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-	return (0);
+	return (1);
 }
