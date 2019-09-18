ed10d168094f42ad6c1f23bd05707e3103715827
AngelPedroza <angelomarpedrozacardenas@gmail.com>
2019-09-18 20:06:14 +0000

    [Fix]Use well betty

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 /**
-*This function is the body that contain all the program
+*main - This function is the body that contain all the program
+*
+*Return: Always is 0
 */
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
-return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
