41993a09821335c9801ba1f14426d836323d1422
AngelPedroza <angelomarpedrozacardenas@gmail.com>
2019-09-19 20:24:11 +0000

    [Add]Same code of 2 excersice

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - The body of the program
+ *
+ * Return: Always is 0 (succes)
+ */
+int main(void)
+{
+	int a;
+
+	for (a = 122; a >= 97; a--)
+	{
+		putchar(a);
+	}
+	putchar('\n');
+	return (0);
+}
