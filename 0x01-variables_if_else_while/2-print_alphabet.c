87de4e5bd394cc7f68045729efa11807c9d21336
AngelPedroza <angelomarpedrozacardenas@gmail.com>
2019-09-19 16:57:14 +0000

    [Add]Use the ascii number

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+/**
+ * main - The body of the program
+ *
+ * Return: Always is 0 (succes)
+ */
+int main(void)
+{
+	int a;
+
+	for (a = 97; a <= 122; a++)
+	{
+		putchar(a);
+	}
+	putchar('\n');
+	return (0);
+}
