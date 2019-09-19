7b4e5efc37c9ec03a2035cda98f52310f8e66a8a
AngelPedroza <angelomarpedrozacardenas@gmail.com>
2019-09-19 20:03:20 +0000

    [Add]putchar is fintction with ascii

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - The body of the program
+ *
+ * Return: Always is 0 (succes)
+ */
+int main(void)
+{
+        int a;
+
+        for (a = 48; a < 58; a++)
+        {
+                putchar(a);
+        }
+        putchar('\n');
+        return (0);
+}
