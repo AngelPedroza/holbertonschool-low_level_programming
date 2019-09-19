490dcd0a49f35f8452d96788771b5548c1090a72
AngelPedroza <angelomarpedrozacardenas@gmail.com>
2019-09-19 17:07:28 +0000

    [Add]Use the ascii number and 2 loops for

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+/**
+ * main - The body of the program
+ *
+ * Return: Always is 0 (succes)
+ */
+int main(void)
+{
+	int a;
+	int b;
+
+	for (a = 97; a <= 122; a++)
+	{
+		putchar(a);
+	}
+	for (b = 64; b <= 90; b++)
+	{
+		putchar(b);
+	}
+	putchar('\n');
+	return (0);
+}
