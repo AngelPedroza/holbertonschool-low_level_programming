037b2884f04ce645bb3738777326395873289b42
AngelPedroza <angelomarpedrozacardenas@gmail.com>
2019-09-19 20:36:01 +0000

    [Add]Use 2 putchar for display lettres and other for display the numbers

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - The body of the program
+ *
+ * Return: Always is 0 (succes)
+ */
+int main(void)
+{
+	int a;
+
+	for (a = 48; a <= 57; a++)
+	{
+		putchar(a);
+	}
+	for (a = 97; a <= 102; a++)
+	{
+		putchar(a);
+	}
+	putchar('\n');
+	return (0);
+}
