802a0f6e8b25a043941b32a89ac547f3d2ca7c5c
AngelPedroza <angelomarpedrozacardenas@gmail.com>
2019-09-19 22:54:02 +0000

    [Add]b = a + 1 for count

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+/**
+ * main - The body of the program
+ *
+ * Return: Always is 0 (succes)
+ */
+int main(void)
+{
+	int a;
+	int b;
+
+	b = 48;
+	while (b < 58)
+	{
+	a = b + 1;
+		while (a < 58)
+		{
+		putchar(b);
+		putchar(a);
+			if (b != 56 || a != 57)
+			{
+			putchar(',');
+			putchar(32);
+			}
+		a++;
+		}
+	b++;
+	}
+	putchar('\n');
+	return (0);
+}
