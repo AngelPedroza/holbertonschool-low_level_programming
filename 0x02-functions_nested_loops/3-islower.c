2d82273e8c9c060c880eeebecccfe1e5596b397b
AngelPedroza <angelomarpedrozacardenas@gmail.com>
2019-09-23 22:40:31 +0000

    [Fix]@variable:

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,13 +1,15 @@
 #include "holberton.h"
 
 /**
- * main - check the code for Holberton School students.
+ * _islower - found the lower characters.
+ *
+ * @c: c is the variable for the program
  *
  * Return: Always 0.
  */
 int _islower(int c)
 {
-	if (c > 97 && c < 122)	
+	if (c > 97 && c < 122)
 	return (1);
 	else
 	return (0);
