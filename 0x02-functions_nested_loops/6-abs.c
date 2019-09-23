9a5f350c03b51e7c75394dc4b6b22b9b1e078141
AngelPedroza <angelomarpedrozacardenas@gmail.com>
2019-09-23 23:53:53 +0000

    [Add]Remember use the holberton.h in all your files

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+int _abs(int i)
+{
+	if (i < 0)
+	i = i * -1;
+	return (i);
+}
