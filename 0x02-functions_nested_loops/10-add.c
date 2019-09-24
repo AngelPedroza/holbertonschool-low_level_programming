c4aa6fe235e46d1f806a2d399b8c2a3b7ed97c55
AngelPedroza <angelomarpedrozacardenas@gmail.com>
2019-09-24 00:44:00 +0000

    [Add]EZ

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+/**
+ * add - check the code for Holberton School students.
+ *
+ * @a: Variable of the function
+ *
+ * @b: Variable of the function
+ *
+ * Return: Always 0.
+ */
+int add(int a, int b)
+{
+	int n;
+
+	n = a + b;
+	return (n);
+}
