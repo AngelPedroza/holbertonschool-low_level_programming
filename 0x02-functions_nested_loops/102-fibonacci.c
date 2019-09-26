7d810b10fc0d55d99c41e6c80a5908f62ad28a29
AngelPedroza <angelomarpedrozacardenas@gmail.com>
2019-09-26 00:37:42 +0000

    [Add] Use usigned and long for make more big my sapace in storage

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - principal function.
+ *
+ * Return: Always 0 (succes)
+ */
+int main(void)
+{
+	unsigned long int a, b, c ,d;
+
+	b = 0;
+	c = 1;
+	d = 0;
+	for (a = 1; a < 50; a++)
+	{
+		d = b + c;
+		printf("%lu\n",d);
+		b = c;
+		c = d;
+	}
+	return (0);
+}
