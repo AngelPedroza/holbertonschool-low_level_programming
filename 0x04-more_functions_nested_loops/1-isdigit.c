1e96bcafebf1a8b2a6528a3aed4973ceda7fe137
AngelPedroza <angelomarpedrozacardenas@gmail.com>
2019-09-26 14:48:17 +0000

    [Add] If is digit return 1, else return 0

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * _isdigit - Identufy if is a digit.
+ * @c: is the variable that I will save my valeu.
+ * Return: Always 0.
+ */
+int _isdigit(int c)
+{
+	if (c > 47 && c < 57)
+		return (1);
+	else
+		return (0);
+	return (c);
+}
