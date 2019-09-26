e35615b8fd7a909491c7a09a9e07902f72606eb8
AngelPedroza <angelomarpedrozacardenas@gmail.com>
2019-09-26 14:58:30 +0000

    [Add] Create a variable for save the valeo of the multiplie

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * mul - multiplies 2 numbers.
+ * @a: The first number for multiplie
+ * @b: The second number to multiplie
+ * Return: the final valeu befor to multiplie.
+ */
+int mul(int a, int b)
+{
+	int c;
+
+	c = a * b;
+	return (c);
+}
