482a7d9aee44f4af43f56072ff00cee5da8f7549
AngelPedroza <angelomarpedrozacardenas@gmail.com>
2019-09-27 21:05:41 +0000

    [Add]Try again tomorrow

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,38 +1,32 @@
 #include "holberton.h"
-/*
-
-
-
+/**
+ * print_number - Display a number.
+ *
+ * Return: Not have cause is void function
  */
 void print_number(int n)
 {
+	int a = 1;
+
 	if (n < 0)
 	{
-		n = n * -1;
 		_putchar('-');
-		_putchar(n / 10 + '0');
-		_putchar(n % 10 + '0');
+		n = n * -1;
 	}
-	else if (n <= 9 || n == 0)
+	else if (n == 0)
 	{
 		_putchar(n + '0');
 	}
-	else if (n <= 99)
+
+	while (n / a > 9)
 	{
-		_putchar(n / 10 + '0');
-		_putchar(n % 10 + '0');
+		a = a * 10;
+		while (a >= 1)
+		{
+			_putchar(n / a + '0');
+			n = n % a;
+			a = a / 10;
+		}
 	}
-	else if (n <= 999)
-	{
-		_putchar(n / 100 + '0');
-		_putchar((n % 100) / 10 + '0');
-		_putchar(n % 10 + '0');
-	}
-	else if (n >= 1000)
-	{
-		_putchar(n / 1000 + '0');
-		_putchar((n / 100) % 10 + '0');
-		_putchar((n % 100) / 10 + '0');
-		_putchar((n % 100) % 10 + '0');
-	}
+       	return;
 }
