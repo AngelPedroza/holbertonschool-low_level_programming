7579387ee2ce68af05579d61579c07ee6c810ae3
AngelPedroza <angelomarpedrozacardenas@gmail.com>
2019-09-30 15:34:05 +0000

    [Add]*n change the valeu

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,10 @@
+#include "holberton.h"
+/**
+ * reset_to_98 - change the valeu of a int variable
+ * @n: The variable that is a pinter
+ * Return: Not have
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+This dir is for make excercises for pointer in c
