f300db5455ffea5ebf74714163745f9e18787c3d
AngelPedroza <angelomarpedrozacardenas@gmail.com>
2019-09-30 16:00:22 +0000

    [Add]*a *b change the valeu

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,12 @@
+#include "holberton.c"
+/**
+ * swap_int - Change the value of two variables
+ * @a: Variable
+ * @b: Variable
+ * Return: Not have
+ */
+void swap_int(int *a, int *b)
+{
+	*a = 42;
+	*b = 98;
+}
