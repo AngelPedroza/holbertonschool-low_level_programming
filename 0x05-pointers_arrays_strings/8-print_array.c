9f5cbfa8b2febcf0ae5b77c113072727e02ceb3b
AngelPedroza <angelomarpedrozacardenas@gmail.com>
2019-10-01 20:56:20 +0000

    [Fix] Think in all the cases of n

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -10,9 +10,9 @@ void print_array(int *a, int n)
 {
 	int b;
 
-	for (; b < n; b++)
+	for (b = 0; b < n; b++)
 	{
-		if (b == (n - 1))
+	        if (b == (n - 1))
 		{
 			printf("%d\n", a[b]);
 		}
@@ -21,4 +21,8 @@ void print_array(int *a, int n)
 			printf("%d, ", a[b]);
 		}
 	}
+		if (n == 0 || n < 0)
+		{
+			printf("\n");
+		}
 }
