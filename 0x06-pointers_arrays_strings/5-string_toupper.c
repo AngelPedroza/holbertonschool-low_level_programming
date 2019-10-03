ff877fed3f8a60899ca3f366aa7275c40076315c
AngelPedroza <angelomarpedrozacardenas@gmail.com>
2019-10-03 22:38:08 +0000

    [Add] Change all the lower cases in uppers letters

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * string_toupper - check the code for Holberton School students.
+ * @s: the char that change all the lowers in uppers
+ * Return: The new char.
+ */
+char *string_toupper(char *s)
+{
+	int a;
+
+	for (; s[a] != '\0'; a++)
+	{
+		if (s[a] >= 97 && s[a] <= 122)
+		{
+			s[a] = s[a] - 32;
+		}
+	}
+	return (s);
+}
