76b0f74e84380533d39ba600033a5bdd7378df9f
AngelPedroza <angelomarpedrozacardenas@gmail.com>
2019-10-07 17:09:12 +0000

    [Fix] If src is null

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -6,13 +6,13 @@
  * @dest: the address of memory to print
  * @src: the size of the memory to print
  * @n: size to replace.
- * Return: Nothing.
+ * Return: Retrun the destiny char.
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int i;
 
-	for (i = 0; i < n && src[i] != '\0'; i++)
+	for (i = 0; i < n; i++)
 	{
 		dest[i] = src[i];
 	}
