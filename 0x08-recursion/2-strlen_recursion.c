8f2d7b5e52afdb1a10b4a24d9758c442ebf0374b
AngelPedroza <angelomarpedrozacardenas@gmail.com>
2019-10-09 17:13:11 +0000

    [Add] use the return for make the recursion

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return(0);
+
+	}
+	s++;
+	return(_strlen_recursion(s) + 1);
+}
