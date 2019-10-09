34ceea5ef1e47fe9484bc7963be39775f6393e25
AngelPedroza <angelomarpedrozacardenas@gmail.com>
2019-10-09 20:11:31 +0000

    [Fix] Fix betty for two functions

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -2,11 +2,12 @@
 #include <stdio.h>
 
 /**
- * main - check the code for Holberton School students.
- *
- * Return: Always 0.
+ * _number - My function for make my conditional of a sqrt.
+ * @a: My number to find the sqrs
+ * @y: My cont.
+ * Return: Result of my operation.
  */
-int _number(int a , int y)
+int _number(int a, int y)
 {
 	if ((y * y) != a && (y * y) < a)
 	{
@@ -18,6 +19,11 @@ int _number(int a , int y)
 	}
 	return (-1);
 }
+/**
+ * _sqrt_recursion - Print my number for sqrt.
+ * @n: My nomber for search her sqrt
+ * Return: The number.
+ */
 int _sqrt_recursion(int n)
 {
 	int b;
