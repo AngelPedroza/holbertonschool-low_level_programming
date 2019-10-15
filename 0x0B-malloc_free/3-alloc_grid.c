3e38839be5c4aae7a29bc13ea547629a5c341d77
AngelPedroza <angelomarpedrozacardenas@gmail.com>
2019-10-15 21:22:03 +0000

    [Add] If all the number are inicialized in 0

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,35 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * print_grid - prints a grid of integers
+ * @grid: the address of the two dimensional grid
+ * @width: width of the grid
+ * @height: height of the grid
+ *
+ * Return: Nothing.
+ */
+int **alloc_grid(int width, int height)
+{
+	int **a;
+	int i, j;
+
+	if (width <= 0 || height <= 0)
+		return (0);
+
+	a = (int **)malloc((height * sizeof(int))* width);
+
+	if (a == NULL)
+		return (0);
+
+	for (i = 0; i <= height; i++)
+	{
+		*(a + i) = (int *)malloc(width * sizeof(int));
+		for (j = 0; j <= width; j++)
+	{
+		a[i][j] = 0;
+	}
+}
+return (a);
+}
