74f2124750fb4a9d53bb79446947096319f03d07
AngelPedroza <angelomarpedrozacardenas@gmail.com>
2019-10-15 18:44:49 +0000

    [Add] My first try with malloc

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * create_array - create a dinamic array and change an value.
+ * @c: My change for the array
+ * @size: the size of the memory to reserve.
+ *
+ * Return: Nothing.
+ */
+char *create_array(unsigned int size, char c)
+{
+	char *a;
+	unsigned int i;
+
+	if (size == 0)
+	{
+		return (0);
+	}
+	a = (char *)malloc(size * sizeof(c));
+	for (i = 0; i <= size; i++)
+		a[i] = 'H';
+
+	return (a);
+}
---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,40 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * _strlen - Give me the length of a string.
+ * @str: Ny string of input.
+ * Return: The length.
+ */
+int _strlen(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{}
+	i--;
+	return (i);
+}
+/**
+ * _strdup - Copy an string in a new space of memory declarated before for
+ * malloc.
+ * @str: Ny string of input.
+ * Return: The coppy of the string.
+ */
+char *_strdup(char *str)
+{
+	char *a;
+	int j, k;
+
+	if (str == NULL)
+		return (0);
+
+	j = _strlen(str);
+	a = (char *)malloc(j * sizeof(char));
+	for (k = 0; str[k] != '\0'; k++)
+	{
+		a[k] = str[k];
+	}
+	return (a);
+}
