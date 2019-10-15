867b596604ac4e22ccd99baaf1a6864de5f47e57
AngelPedroza <angelomarpedrozacardenas@gmail.com>
2019-10-15 22:28:45 +0000

    [Add] I hate the checkers

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -3,12 +3,11 @@
 #include <stdlib.h>
 
 /**
- * print_grid - prints a grid of integers
- * @grid: the address of the two dimensional grid
+ * alloc_grid - print a array
  * @width: width of the grid
  * @height: height of the grid
  *
- * Return: Nothing.
+ * Return: doble pointer for the bidimensional array.
  */
 int **alloc_grid(int width, int height)
 {
@@ -18,7 +17,7 @@ int **alloc_grid(int width, int height)
 	if (width <= 0 || height <= 0)
 		return (0);
 
-	a = (int **)malloc((height * sizeof(int))* width);
+	a = (int **)malloc((height * sizeof(int)) * width);
 
 	if (a == NULL)
 		return (0);
@@ -27,9 +26,9 @@ int **alloc_grid(int width, int height)
 	{
 		*(a + i) = (int *)malloc(width * sizeof(int));
 		for (j = 0; j <= width; j++)
-	{
-		a[i][j] = 0;
+		{
+			a[i][j] = 0;
+		}
 	}
-}
-return (a);
+	return (a);
 }
