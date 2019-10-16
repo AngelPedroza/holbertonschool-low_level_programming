f681c102158aaa31cf8721d8fdbda17dbe35b211
AngelPedroza <angelomarpedrozacardenas@gmail.com>
2019-10-16 02:10:04 +0000

    [Fix] <= and >=

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -27,16 +27,16 @@ int **alloc_grid(int width, int height)
 	for (i = 0; i < height; i++)
 	{
 		*(a + i) = (int *)malloc(width * sizeof(int));
-		/* if (a[i] == NULL) */
-		/* { */
-		/* 	for (i = width; i >= 0; i--) */
-		/* 	{ */
-		/* 		free(a[i]); */
-		/* 	} */
-		/* 	free(a); */
-		/* 	a = NULL; */
-		/* 	return (a); */
-		/* } */
+		if (a[i] == NULL)
+		{
+			for (i = width; i >= 0; i--)
+			{
+				free(a[i]);
+			}
+			free(a);
+			a = NULL;
+			return (a);
+		}
 		for (j = 0; j < width; j++)
 		{
 			a[i][j] = 0;
