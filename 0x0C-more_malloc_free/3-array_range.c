0ef4cdd142fdd8871a10565ada00b8e1531799ec
AngelPedroza <angelomarpedrozacardenas@gmail.com>
2019-10-17 20:55:52 +0000

    [Fix] The loop

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -15,20 +15,19 @@ int *array_range(int min, int max)
 	int *p;
 	int i, size;
 
-	size = (max - min) + 2;
+	size = (max - min) + 1;
 
 	if (min > max)
 		return (NULL);
-	if (max == min)
-		size = 2;
+
 	p = malloc((sizeof(int) * size));
 
 	if (p == NULL)
 		return (NULL);
 
-	for (i = min; i <= max; i++)
+	for (i = 0; min <= max; i++)
 	{
-		p[i] = i;
+		p[i] = min++;
 	}
 	return (p);
 }
