501e96ae5b414a85aaceacd9c404cd12532631ca
AngelPedroza <angelomarpedrozacardenas@gmail.com>
2019-10-18 00:11:32 +0000

    [Fix] Change if new_size if > or <

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -43,22 +43,24 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 		else
 			return (ptr);
 	}
-	p = malloc(new_size);
-
-	if (p == NULL)
-		return (NULL);
 	if (new_size == 0 && ptr != NULL)
 	{
 		free(ptr);
 		return (NULL);
 	}
+
+	p = malloc(new_size);
+
+	if (p == NULL)
+		return (NULL);
+
 	if (new_size < old_size)
 	{
-		_memcpy(p, ptr, old_size);
+		_memcpy(p, ptr, new_size);
 	}
 	else if (new_size > old_size)
 	{
-		_memcpy(p, ptr, new_size);
+		_memcpy(p, ptr, old_size);
 	}
 	free(ptr);
 	return (p);
