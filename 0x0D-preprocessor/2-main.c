b709ca75faba8095c2fd8e835e9bc54a014dacde
AngelPedroza <angelomarpedrozacardenas@gmail.com>
2019-10-21 18:50:28 +0000

    [Add] Use macros for give values

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * main - my funtion to print the nme of my file.
+ * Return: Always 0.
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
---
+++ 0x0D-preprocessor/README.md
@@ -0,0 +1,1 @@
+# This repo is for know the macros and other uses of the headers
