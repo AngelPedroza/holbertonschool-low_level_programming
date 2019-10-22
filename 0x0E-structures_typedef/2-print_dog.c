85fc4c19e3600ab94af4c4eeebecf4484c71edff
AngelPedroza <angelomarpedrozacardenas@gmail.com>
2019-10-22 01:57:48 +0000

    [Fix] Add \n after of nil

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -11,10 +11,10 @@ void print_dog(struct dog *d)
 	if (d)
 	{
 		d->name == NULL ?
-			printf("Name: (nil)") : printf("Name: %s\n", d->name);
-		d->age == 0 ?
-			printf("Age: (nil)") : printf("Age: %f\n", d->age);
+			printf("Name: (nil)\n") : printf("Name: %s\n", d->name);
+		d->age <= 0 ?
+			printf("Age: (nil)\n") : printf("Age: %f\n", d->age);
 		d->owner == NULL ?
-			printf("Owner: (nil)") : printf("Owner: %s\n", d->owner);
+			printf("Owner: (nil)\n") : printf("Owner: %s\n", d->owner);
 	}
 }
