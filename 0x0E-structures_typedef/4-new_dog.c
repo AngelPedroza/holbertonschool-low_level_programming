7e236a8bb5a79837bd74c571ccfe71a4ecead6a8
AngelPedroza <angelomarpedrozacardenas@gmail.com>
2019-10-22 02:20:39 +0000

    [Fix] was Copyowner not copyname

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,11 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "dog.h"
-
+/**
+ * countandreplace - Count and save a string in a malloc.
+ * @str: My string.
+ * Return: New pointer of malloc.
+ */
 void *countandreplace(char *str)
 {
 	int i;
 	char *p;
+
 	for(i = 0; str[i] != '\0'; i++)
 		;
 	p = malloc(i + 1);
@@ -17,15 +22,17 @@ void *countandreplace(char *str)
 	return (p);
 }
 /**
- * main - check the code for Holberton School students.
- *
- * Return: Always 0.
+ * new_dog - Create a copy of my structure.
+ * @name: Name of my dog
+ * @age: Age of my dog
+ * @owner: Owner of my dog
+ * Return: New pointer.
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	char *copyname, *copyowner;
-
 	dog_t *dog_m;
+
 	dog_m = malloc(sizeof(dog_t));
 	if (dog_m == NULL)
 		return (NULL);
@@ -36,7 +43,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 		return (NULL);
 		}
 	copyowner = countandreplace(owner);
-	if (copyname == NULL)
+	if (copyowner == NULL)
 	{
 		free(dog_m);
 		free(copyname);
