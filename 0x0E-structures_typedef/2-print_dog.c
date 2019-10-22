a7303936cbdac3186f8c8bd3b1dbdee8e152cf12
AngelPedroza <angelomarpedrozacardenas@gmail.com>
2019-10-22 01:52:32 +0000

    [Fix] Add Null with age

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -12,7 +12,7 @@ void print_dog(struct dog *d)
 	{
 		d->name == NULL ?
 			printf("Name: (nil)") : printf("Name: %s\n", d->name);
-		d->age == NULL ?
+		d->age == 0 ?
 			printf("Age: (nil)") : printf("Age: %f\n", d->age);
 		d->owner == NULL ?
 			printf("Owner: (nil)") : printf("Owner: %s\n", d->owner);
