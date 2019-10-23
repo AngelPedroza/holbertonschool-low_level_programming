70f8982b492ca84b2eb74ce2d2c43143b7bafedd
AngelPedroza <angelomarpedrozacardenas@gmail.com>
2019-10-23 19:06:55 +0000

    [Fix] If the parameters are NULll and the rest of conditions

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -2,21 +2,25 @@
 #include "function_pointers.h"
 
 /**
- * is_98 - check if a number is equal to 98
- * @elem: the integer to check
- *
+ * int_index - check if a number has match
+ * @array: Array to check
+ * @size: Size of my array.
+ * @cmp: My pointer to functions
  * Return: 0 if false, something else otherwise.
  */
 int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i, index;
 
-	index = 0;
-	for (i = 0; i < size; i++)
+	if (size >= 0)
 	{
-		index = cmp(array[i]);
-		if (index == 1)
-			break;
+		index = 0;
+		for (i = 0; i < size; i++)
+		{
+			index = cmp(array[i]);
+			if (index == 1)
+				return (i);
+		}
 	}
-	return (i);
+	return (-1);
 }
