ad5ef6309cfdebf639be70381b1511b3ef396695
AngelPedroza <angelomarpedrozacardenas@gmail.com>
2019-10-23 18:31:15 +0000

    [Fix] If the parameters are NULL

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -2,13 +2,14 @@
 #include "function_pointers.h"
 
 /**
- * print_name_as_is - prints a name as is
+ * print_name - prints a name called another function
  * @name: name of the person
- *
+ * @f: My pointer to function.
  * Return: Nothing.
  */
 
 void print_name(char *name, void (*f)(char *))
 {
-	f(name);
+	if (name && f)
+		f(name);
 }
