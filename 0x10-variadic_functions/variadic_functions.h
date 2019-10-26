7af912093f70c19e004b5d2da98c5cb6fdc53544
AngelPedroza <angelomarpedrozacardenas@gmail.com>
2019-10-26 17:55:27 +0000

    [Add] Add commits for the structure

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -2,11 +2,17 @@
 #define VARIADIC
 #include <stdio.h>
 #include <stdarg.h>
+/**
+ * struct select - Structure for other structure
+ *
+ * @c: The match character
+ * @f: The function associated and pass a parameter
+ */
 typedef struct select
 {
 	char c;
 	void (*f)(va_list listed);
-}op_t;
+} op_t;
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
