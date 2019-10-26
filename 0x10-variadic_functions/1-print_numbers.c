9ad99127735b7ca71bf8b60363e7beb54bcd0171
AngelPedroza <angelomarpedrozacardenas@gmail.com>
2019-10-26 20:22:48 +0000

    [Fix] Change ifs

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -14,12 +14,11 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	va_start(my_list, n);
 	for (i = 0; i < n; i++)
 	{
-		if (i != n - 1 && separator != NULL)
-			printf("%d%s", va_arg(my_list, int), separator);
-		else if (i != n - 1 && separator == NULL)
+		if (separator == NULL)
 			printf("%d", va_arg(my_list, int));
-		else if (i == n - 1)
-			printf("%d\n", va_arg(my_list, int));
+		else if (i < n - 1 && separator != NULL)
+			printf("%d%s", va_arg(my_list, int), separator);
 	}
+	printf("\n");
 	va_end(my_list);
 }
