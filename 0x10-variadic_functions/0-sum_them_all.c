b15f01180343cbbd3da54e99ea7fe270ec7d61b8
AngelPedroza <angelomarpedrozacardenas@gmail.com>
2019-10-26 17:50:59 +0000

    [Fix] Move the declarations of the variables

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -7,11 +7,11 @@
  */
 int sum_them_all(const unsigned int n, ...)
 {
+	va_list list;
+	unsigned int i, sum;
+
 	if (n != 0)
 	{
-		va_list list;
-		unsigned int i, sum;
-
 		sum = 0;
 		va_start(list, n);
 		for (i = 0; i < n; i++)
