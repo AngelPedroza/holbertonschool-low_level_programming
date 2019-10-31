340204562ef8c57465b5e58a0abe03c9b09766db
AngelPedroza <angelomarpedrozacardenas@gmail.com>
2019-10-31 21:05:09 +0000

    [Fix] If head is NULL

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -1,12 +1,14 @@
 #include "lists.h"
 
 /**
- * free_list -
- *
+ * free_list - free all nodes and each strdup.
+ * @head: The address of the node
  * Return: Nothing
  */
 void free_list(list_t *head)
 {
+	if (head == NULL)
+		return;
 	if (head->next != NULL)
 		free_list(head->next);
 	free(head->str);
