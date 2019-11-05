7f813ed0e28e77ee9cbf5e89be01c2b200a64331
AngelPedroza <angelomarpedrozacardenas@gmail.com>
2019-11-05 22:31:33 +0000

    [Add] Free the nodes

---
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -0,0 +1,31 @@
+#include "lists.h"
+
+/**
+ * free_listint_safe - print a list (nodes) of integers.
+ * @h: Address of each node
+ * Return: How many nodes I have.
+ */
+size_t free_listint_safe(listint_t **h)
+{
+	int i = 0;
+	listint_t *tmp;
+
+	if (*h)
+	{
+		tmp = *h;
+		for (i = 0; *h != NULL; i++)
+		{
+			*h = (*h)->next;
+			if (tmp < *h)
+			{
+				i++;
+				free(tmp);
+				*h = NULL;
+				break;
+			}
+			free(tmp);
+			tmp = *h;
+		}
+	}
+	return (i);
+}
