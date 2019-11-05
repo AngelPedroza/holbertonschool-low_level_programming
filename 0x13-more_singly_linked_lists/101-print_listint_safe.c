dbbfc86174500dab3f7e7a7c97fa122d56ce0da3
AngelPedroza <angelomarpedrozacardenas@gmail.com>
2019-11-05 21:53:19 +0000

    [Add] If the tmp is < to head than break the loop

---
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -0,0 +1,30 @@
+#include "lists.h"
+
+/**
+ * print_listint - print a list (nodes) of integers
+ * @h: Address of each node
+ * Return: How many nodes I have.
+ */
+size_t print_listint_safe(const listint_t *head)
+{
+	int i;
+	listint_t *tmp;
+
+	if (head)
+	{
+		tmp = (void *)head;
+		for (i = 0; head != NULL; i++)
+		{
+			head = head->next;
+			printf("[%p] %d\n", (void *)tmp, tmp->n);
+			if (tmp < head)
+			{
+				printf("-> [%p] %d\n", (void *)head, head->n);
+				break;
+			}
+			tmp = tmp->next;
+		}
+		return (i);
+	}
+	exit(98);
+}
