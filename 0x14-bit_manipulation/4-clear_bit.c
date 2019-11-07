66c88b5421e4b82dc7f1ae7e6d70c544708bd2f5
AngelPedroza <angelomarpedrozacardenas@gmail.com>
2019-11-07 22:36:38 +0000

    [Fix] If I have a index more than my first bit

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -9,9 +9,13 @@
  */
 int clear_bit(unsigned long int *n, unsigned int index)
 {
+	int i = 1;
+
 	if (index > (sizeof(*n) * 8))
 		return (-1);
-	*n = *n >> (index + 1);
-	*n = *n << (index + 1);
+	*n = ~*n;
+	i <<= (index);
+	*n |= i;
+	*n = ~*n;
 	return (1);
 }
