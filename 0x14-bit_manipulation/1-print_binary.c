6aeadd6d6bccafa0d70858efc675df38e99a7009
AngelPedroza <angelomarpedrozacardenas@gmail.com>
2019-11-07 23:36:12 +0000

    [Fix] Use less lines

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,38 +1,13 @@
-#include <stdio.h>
 #include "holberton.h"
 
 /**
- * print_rev - print each bit in recursively.
- * @n: My number to convert.
- * @checker: if my number is > than 0.
- * Return: Always 0.
- */
-void print_rev(unsigned long int n, int checker)
-{
-	if (checker == 1)
-	{
-		_putchar('0');
-		return;
-	}
-
-	if (n == 0)
-		return;
-
-	print_rev(n >> 1, checker);
-	if ((n & 1) == 0)
-		_putchar('0');
-	if ((n & 1) == 1)
-		_putchar('1');
-}
-/**
- * print_binary - printme a binary od enteger..
+ * print_binary - printme a binary od enteger.
  * @n: My number to convert.
  * Return: Always 0.
  */
 void print_binary(unsigned long int n)
 {
-	if (n == '0')
-		print_rev(n, 1);
-	else
-		print_rev(n, 0);
+	if (n > 1)
+		print_binary(n>>1);
+	_putchar((n & 1) + '0');
 }
