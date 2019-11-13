12acd1f3b2f1405f4095b6c693b89ebb81bcaf0b
AngelPedroza <angelomarpedrozacardenas@gmail.com>
2019-11-13 00:44:27 +0000

    [Fix] Change the library

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,4 +1,9 @@
-#include "holberton.h"
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
 
 /**
  * main - Copy the content of a file in another file.
@@ -9,23 +14,24 @@
 int main(int argc, char *argv[])
 {
 	int fgto, fgfrom, check_close_from, check_close_to;
-	char *buffer;
-	ssize_t checker_write, checker_read;
+	int checker_write, checker_read;
+	char buffer[1024];
 
 	if (argc != 3)
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
-	buffer = malloc(1024);
+
 	fgfrom = open(argv[1], O_RDONLY);
 	if (fgfrom == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
+
 	fgto = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 	if (fgto == -1)
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
-	checker_read = read(fgfrom, (char *)buffer, 1024);
-	while (checker_read > 0)
+
+	while ((checker_read = read(fgfrom, buffer, 1024)) > 0)
 	{
 		checker_write = write(fgto, buffer, checker_read);
 		if (checker_write != checker_read)
@@ -39,12 +45,12 @@ int main(int argc, char *argv[])
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
+
 	check_close_from = close(fgfrom);
 	if (check_close_from == -1)
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fgfrom), exit(100);
 	check_close_to = close(fgto);
 	if (check_close_to == -1)
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fgto), exit(100);
-	free(buffer);
 	return (0);
 }
