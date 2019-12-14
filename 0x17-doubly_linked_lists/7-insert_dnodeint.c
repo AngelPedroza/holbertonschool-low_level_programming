9d27b21c8347ae9fb588fbfc1eb8483d96a36a84
AngelPedroza <angelomarpedrozacardenas@gmail.com>
2019-12-14 23:31:32 +0000

    [Fix] Fix bety style

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -15,10 +15,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 
 	new_node = malloc(sizeof(dlistint_t));
 	if (new_node == NULL)
-	{
-		free(new_node);
-		return (NULL);
-	}
+		return (free(new_node), NULL);
 
 	tmp = *h;
 	new_node->n = n;
@@ -26,25 +23,17 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	new_node->prev = NULL;
 
 	if (*h == NULL && idx > 0)
-	{
-		free(new_node);
-		return (NULL);
-	}
-
+		return (free(new_node), NULL);
 	if (idx == 0)
 	{
-		new_node->next = *h;
-		*h = new_node;
+		new_node->next = *h, *h = new_node;
 		return (new_node);
 	}
 
 	for (i = 0; i < idx - 1; i++)
 	{
-		if (tmp == NULL)
-		{
-			free(new_node);
-			return (NULL);
-		}
+		if (tmp == NULL && idx - i > 0)
+			return (free(new_node), NULL);
 		tmp = tmp->next;
 	}
 	new_node->next = tmp->next;
