27f017448906dffbd7488d91e5c713b3497379dd
AngelPedroza <angelomarpedrozacardenas@gmail.com>
2019-12-15 00:50:56 +0000

    [Add] First try

---
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -0,0 +1,41 @@
+#include "lists.h"
+
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always EXIT_SUCCESS.
+ */
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
+{
+	dlistint_t *tmp;
+	dlistint_t *tmp2;
+	unsigned int i = 0;
+
+	if (*head && head)
+	{
+		tmp = *head;
+
+		if (index == 0)
+		{
+			*head = tmp->next;
+			free(tmp);
+		}
+		else
+		{
+			while (i != index - 1)
+			{
+				tmp = tmp->next;
+				if (tmp == NULL)
+					return (-1);
+				i++;
+			}
+			tmp2 = tmp->next;
+			tmp->next = tmp->next->next;
+			free(tmp2);
+			tmp2 = tmp->next;
+			tmp2->prev = tmp;
+		}
+		return (1);
+	}
+	return (-1);
+}
