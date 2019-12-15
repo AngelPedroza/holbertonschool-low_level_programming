d9fe977bd7a4171c8be7cb46582782090d503f58
AngelPedroza <angelomarpedrozacardenas@gmail.com>
2019-12-15 01:31:50 +0000

    [Fix] Betty style

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -20,14 +20,11 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		ptrcount = ptrcount->next, i++;
 	if (idx > i)
 		return (NULL);
-
 	new_node = malloc(sizeof(dlistint_t));
 	if (new_node == NULL)
 		return (NULL);
-
 	tmp = *h;
 	new_node->n = n;
-
 	if (idx == 0)
 	{
 		new_node->prev = NULL, new_node->next = *h;
@@ -36,7 +33,6 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		*h = new_node;
 		return (new_node);
 	}
-
 	for (i = 0; i < idx; i++)
 	{
 		tmp2 = tmp;
@@ -45,9 +41,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	new_node->next = tmp;
 	new_node->prev = tmp2;
 	tmp2->next = new_node;
-
 	if (tmp)
 		tmp->prev = new_node;
-
 	return (new_node);
 }
