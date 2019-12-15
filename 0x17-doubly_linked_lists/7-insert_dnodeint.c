7d630c203c4c9ea3fb02506a608f83f1e464d432
AngelPedroza <angelomarpedrozacardenas@gmail.com>
2019-12-15 01:30:12 +0000

    [Fix] Change the logic

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -10,34 +10,44 @@
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
 	dlistint_t *new_node;
-	dlistint_t *tmp;
-	unsigned int i;
+	dlistint_t *tmp, *ptrcount = *h;
+	dlistint_t *tmp2;
+	unsigned int i = 0;
+
+	if (!h)
+		return (NULL);
+	while (ptrcount)
+		ptrcount = ptrcount->next, i++;
+	if (idx > i)
+		return (NULL);
 
 	new_node = malloc(sizeof(dlistint_t));
 	if (new_node == NULL)
-		return (free(new_node), NULL);
+		return (NULL);
 
 	tmp = *h;
 	new_node->n = n;
-	new_node->next = NULL;
-	new_node->prev = NULL;
 
-	if (*h == NULL && idx > 0)
-		return (free(new_node), NULL);
 	if (idx == 0)
 	{
-		new_node->next = *h, *h = new_node;
+		new_node->prev = NULL, new_node->next = *h;
+		if (*h)
+			(*h)->prev = new_node;
+		*h = new_node;
 		return (new_node);
 	}
 
-	for (i = 0; i < idx - 1; i++)
+	for (i = 0; i < idx; i++)
 	{
-		if (tmp == NULL && idx - i > 0)
-			return (free(new_node), NULL);
+		tmp2 = tmp;
 		tmp = tmp->next;
 	}
-	new_node->next = tmp->next;
-	tmp->next = new_node;
-	new_node->prev = tmp;
+	new_node->next = tmp;
+	new_node->prev = tmp2;
+	tmp2->next = new_node;
+
+	if (tmp)
+		tmp->prev = new_node;
+
 	return (new_node);
 }
