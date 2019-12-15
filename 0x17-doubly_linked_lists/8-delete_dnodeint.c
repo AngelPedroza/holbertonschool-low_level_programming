beae312553aef05317ec3cd11be859c354bab2e2
AngelPedroza <angelomarpedrozacardenas@gmail.com>
2019-12-15 01:50:17 +0000

    [Fix] The return value

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -26,7 +26,7 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 			*head = tmp;
 			if (*head != NULL)
 				(*head)->prev = NULL;
-			return (-1);
+			return (1);
 		}
 		for (i = 0; i < index; i++)
 		{
