b03ca9be377a7bc10397ecbbb2fa3a38cb89e808
AngelPedroza <angelomarpedrozacardenas@gmail.com>
2020-01-18 22:09:58 +0000

    [Add] Create the index table

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,17 @@
+#include "hash_tables.h"
+
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *ht;
+
+	ht = malloc(sizeof(hash_table_t));
+	if (ht == NULL)
+		return (NULL);
+
+	ht->array = (hash_node_t **)malloc(size * sizeof(hash_node_t));
+	if (ht->array == NULL)
+		return (NULL);
+
+	ht->size = size;
+	return (ht);
+}
