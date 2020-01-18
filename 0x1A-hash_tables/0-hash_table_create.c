e53d6f1fef6e4d381c8d2f1dc3648274dbe261eb
AngelPedroza <angelomarpedrozacardenas@gmail.com>
2020-01-18 22:18:31 +0000

    [Fix] The pointer to the array

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,5 +1,11 @@
 #include "hash_tables.h"
 
+/**
+ * hash_table_create - create the first array
+ * @size: The sizeof the array(index) of the hash table
+ * Return: The hash table.
+ */
+
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *ht;
@@ -8,7 +14,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 	if (ht == NULL)
 		return (NULL);
 
-	ht->array = (hash_node_t **)malloc(size * sizeof(hash_node_t));
+	ht->array = (hash_node_t **)malloc(size * sizeof(hash_node_t *));
 	if (ht->array == NULL)
 		return (NULL);
 
