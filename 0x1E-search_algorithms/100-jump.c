cdc0650a058b40986f2556d7618379cde700ab15
AngelPedroza <angelomarpedrozacardenas@gmail.com>
2020-04-15 13:32:11 -0500

    [Fix] Change the condition to search the value in the range

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -14,21 +14,20 @@ int jump_search(int *array, size_t size, int value)
 	int i;
 
 
-	if (array == NULL)
+	if (array == NULL || size == 0)
 		return (-1);
-	while ((int)size > end)
+
+	while (end < size && (int)size > end)
 	{
 		printf("Value checked array[%d] = [%d]\n", start, array[start]);
-		if (array[end] >= value)
-			break;
-		if (array[end] <= value)
-		{
-			start = end;
-			end = end + sqrt(size);
-		}
+		start = end;
+		end = end + sqrt(size);
 	}
 	printf("Value found between indexes [%d] and [%d]\n", start, end);
-	for (i = start; i <= end && i < (int)size; i++)
+	if (end > (size - 1)
+	    end = size - 1;
+
+	for (i = start; i <= end && array[i] <= value; i++)
 	{
 		printf("Value checked array[%d] = [%d]\n", i, array[i]);
 		if (array[i] == value)
